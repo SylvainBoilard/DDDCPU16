11779786337f736011baaed62612dcc9d46be077
Sylvain BOILARD <boilard@crans.org>
2012-04-23 10:44:12 +0200

    Added extra warnings for compilation.

--- emulator/compile.bash
+++ emulator/compile.bash
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 CC="gcc"
-FLAGS="-ansi -pedantic"
+FLAGS="-ansi -pedantic -Wall -Wextra -Wmain -Wswitch-default -Wmissing-include-dirs -Wmissing-declarations -Wfloat-equal -Wundef -Wcast-align -Wredundant-decls -Winit-self -Wshadow"
 
 case "$1" in
     "debug")
 	FLAGS="$FLAGS -g" ;;
     ""|"release")
 	FLAGS="$FLAGS -s -O2" ;;
+    *)
+	echo "Unrecognized build type."
+	exit 1
 esac
 
 LINK=1
@@ -22,4 +25,9 @@ done
 
 if [ $LINK -eq 1 ] ; then
     gcc *.o -o DCPU16
+    if [ $? -ne 0 ] ; then
+	exit 1
+    fi
+else
+    exit 1
 fi
